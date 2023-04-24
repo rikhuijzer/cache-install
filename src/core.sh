54ecb51625bab5594b60f54c9a02f2d2a17408a7
rikhuijzer <rikhuijzer@pm.me>
2023-04-24 11:41:34 +0200

    Move `core.sh` to `src`

--- src/core.sh
+++ src/core.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -e
 
@@ -78,7 +78,7 @@ function set_nix_path {
 function prepare {
   echo "Preparing restore"
   sudo mkdir -p --verbose /nix
-  sudo chown --verbose "$USER:" /nix 
+  sudo chown --verbose "$USER:" /nix
 }
 
 function undo_prepare {
