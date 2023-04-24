91d81469f1c7995b37af58da72c57ce1e1cc15f5
rikhuijzer <rikhuijzer@pm.me>
2023-04-24 12:01:54 +0200

    Link profile correctly

--- src/core.sh
+++ src/core.sh
@@ -73,8 +73,7 @@ function set_paths {
 function set_nix_profile_symlink {
   echo "Running set_nix_profile_symlink from core.sh"
 
-  mkdir -p "/home/$USER/.nix-profile"
-  ln -v -s "/nix/var/nix/profiles/per-user/$USER/profile/bin" "/home/$USER/.nix-profile/bin"
+  ln -v -s "/nix/var/nix/profiles/per-user/$USER/profile" "/home/$USER/.nix-profile"
 }
 
 function set_nix_path {
